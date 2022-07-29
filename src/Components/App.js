6c2b9c19666a7ef5318416fe94763727a408e200
karsyn ansari <karsyn.ansari@gmail.com>
2022-07-28 19:40:03 -0600

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import '../index.css'; 
+import Hero from './Hero'
+import Nav from './Nav';
+
+
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+
+function Nav() {
+  return (
+    <>
+      <header>
+        <h1>Pad's Healing Hand Salve</h1>
+        <nav>
+          <ul>
+            <ul>
+              placeholder1
+            </ul>
+            <ul>
+              placholder2
+            </ul>
+            <ul>placeholder3</ul>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+const styledHeader = styled.header`
+  height: 125vh;
+  font-family: sans-serif;
+  color: red;
+`; 
+
+
+export default Nav
+
