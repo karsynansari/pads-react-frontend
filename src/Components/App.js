af46027eaa8752fceb45ab52f2265bcdd4ff1cff
karsyn ansari <karsyn.ansari@gmail.com>
2022-08-12 09:04:51 -0600

    nav bar exists

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import '../App.css';
-import '../index.css'; 
-import Hero from './Hero'
-import Nav from './Nav';
-
+import React, {useState} from "react";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import { RedButton, BlueButton, GreenButton } from "./Button.styled";
 
 
 function App() {
+const [buttonClicks, setButtonClicks] = useState([])
+console.log(buttonClicks)
+
+  function handleClick(e) {
+   setButtonClicks(e.target.value)
+  }
   return (
     <>
-    <Nav/>
-    <Hero/>
+      <Nav />
+      <Hero />
+      <RedButton value={1} onClick={handleClick}>
+        Click Me
+      </RedButton>
+      <BlueButton value={2} onClick={handleClick}>
+        Click Me
+      </BlueButton>
+      <GreenButton value={3} onClick={handleClick}>
+        Click Me
+      </GreenButton>
+  <div>Button {buttonClicks} Was Clicked</div>
     </>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,35 +1,51 @@
-import React from 'react'
-import styled from 'styled-components';
-
-
+import React from "react";
+import styled from "styled-components";
 
 function Nav() {
   return (
-    <>
+    <StyledNav>
       <header>
-        <h1>Pad's Healing Hand Salve</h1>
+        <div>
+          <h1>Pad's Healing Hand Salve</h1>
+        </div>
         <nav>
           <ul>
-            <ul>
-              placeholder1
-            </ul>
-            <ul>
-              placholder2
-            </ul>
+            <ul>placeholder1</ul>
+            <ul>placholder2</ul>
             <ul>placeholder3</ul>
           </ul>
         </nav>
       </header>
-    </>
+    </StyledNav>
   );
 }
 
-const styledHeader = styled.header`
+export const StyledNav = styled.div`
   height: 125vh;
+  background-size: cover;
   font-family: sans-serif;
-  color: red;
-`; 
+  margin-top: 80px;
+  padding: 30px;
 
+  header {
+    background-color: gray;
+    position: fixed;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    h1 {
+      position: center;
+    }
+    nav {
+      color: green;
+    }
+  }
+  header * {
+    display: inline;
+  }
+`;
 
-export default Nav
-
+export default Nav;
